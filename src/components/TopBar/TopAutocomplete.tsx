edc80d6a37598faa97f51d4e6a876506315f9265
jorgeG92 <jorgegc192@gmail.com>
2023-06-24 13:38:37 +0200

    Fix Autocomplete error console and create their own file

---
+++ src/components/TopBar/TopAutocomplete.tsx
@@ -0,0 +1,104 @@
+import { FC, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../store';
+import { GECODE_Location } from '../../api/locations';
+import { fetchLocations, setLocation } from '../../store/locations';
+import { Autocomplete, CircularProgress, TextField } from '@mui/material';
+import styled from '@emotion/styled';
+
+const AutocompleteContainer = styled.div({
+  flexGrow: 1,
+  maxWidth: 500,
+  minWidth: 50,
+  backgroundColor: 'white',
+  borderRadius: 5,
+});
+
+const parseLocationName = (
+  locationName: GECODE_Location['display_name']
+): string => {
+  const placesArray = locationName.split(', ');
+  return [placesArray[0], placesArray[1], placesArray.slice(-2)].join(', ');
+};
+
+const TopAutocomplete: FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { loading: loadingLocations, locations } = useSelector(
+    (state: RootState) => state.locations
+  );
+
+  // Keep selected location value, all object
+  const [selectedValue, setSelectedValue] = useState<
+    GECODE_Location | undefined
+  >(undefined);
+
+  // Keeps the typed value
+  const [inputValue, setInputValue] = useState<string>('');
+
+  // This onChange is called when the user, at least, selects one option
+  const onChangeAutoComplete = (
+    _: any,
+    value: GECODE_Location | string | null
+  ) => {
+    if (value && typeof value !== 'string') {
+      setSelectedValue(value);
+      dispatch(setLocation(value));
+    }
+  };
+
+  // This onChange is called when the user is typing an fetch new options
+  const onInputChange = (_: any, value: string) => {
+    if (selectedValue?.display_name !== value) {
+      dispatch(fetchLocations(value));
+      setInputValue(value);
+    }
+  };
+
+  return (
+    <AutocompleteContainer>
+      <Autocomplete
+        size="small"
+        // This options is used when locations doesn't fit with default option object of Autocomplete
+        freeSolo
+        loading={loadingLocations}
+        options={locations}
+        value={selectedValue}
+        inputValue={inputValue}
+        onChange={onChangeAutoComplete}
+        onInputChange={onInputChange}
+        // Set html label for each option
+        getOptionLabel={(option) =>
+          option && typeof option !== 'string' ? option.display_name : ''
+        }
+        // Set condition to know if two options are equal
+        isOptionEqualToValue={(option, value) =>
+          option.place_id === value.place_id
+        }
+        // Add unique key in HTML DOM an show parsed name option
+        renderOption={(props, option) => (
+          <li {...props} key={option.place_id}>
+            {parseLocationName(option.display_name)}
+          </li>
+        )}
+        // Component used to render on Autocomplete
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            placeholder="Buscar ciudad"
+            // Add circular progress when its fetching locations
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: loadingLocations ? (
+                <CircularProgress size={20} />
+              ) : null,
+            }}
+          />
+        )}
+        // Disabled native filtering of component suggested by MUI -> https://mui.com/material-ui/react-autocomplete/#search-as-you-type
+        filterOptions={(x) => x}
+      />
+    </AutocompleteContainer>
+  );
+};
+
+export default TopAutocomplete;
--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,58 +1,18 @@
 import styled from '@emotion/styled';
-import {
-  AppBar,
-  Autocomplete,
-  LinearProgress,
-  TextField,
-  Toolbar,
-  Typography,
-} from '@mui/material';
-import { FC, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { GECODE_Location } from '../../api/locations';
-import { AppDispatch, RootState } from '../../store';
-import { fetchLocations, setLocation } from '../../store/locations';
+import { AppBar, LinearProgress, Toolbar, Typography } from '@mui/material';
+import { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
+import TopAutocomplete from './TopAutocomplete';
 
 const JustifyToolbar = styled(Toolbar)({
   justifyContent: 'space-between',
 });
 
-const AutocompleteContainer = styled.div({
-  flexGrow: 1,
-  maxWidth: 500,
-  minWidth: 50,
-  backgroundColor: 'white',
-  borderRadius: 5,
-});
-
 const TopBar: FC = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const { loading: loadingLocations, locations } = useSelector(
-    (state: RootState) => state.locations
-  );
   const { loading: loadingWeatherInfo } = useSelector(
     (state: RootState) => state.weather
   );
-
-  const [selectedValue, setSelectedValue] = useState<
-    GECODE_Location | undefined
-  >(undefined);
-  const [inputValue, setInputValue] = useState<string>('');
-
-  const onChangeAutoComplete = (_: any, value: GECODE_Location | null) => {
-    // Una vez seleccionado buscamos la información del tiempo
-    // No hace falta que este controlado tanto en Redux como React
-    if (value) {
-      setSelectedValue(value);
-      dispatch(setLocation(value));
-    }
-  };
-
-  const onInputChange = (_: any, value: string) => {
-    // Sirve para pedir las peticiones a la API de localizaciones
-    dispatch(fetchLocations(value));
-    setInputValue(value);
-  };
 
   return (
     <AppBar position="relative">
@@ -60,26 +20,7 @@ const TopBar: FC = () => {
         <Typography variant="h5" noWrap>
           Weather App
         </Typography>
-        <AutocompleteContainer>
-          <Autocomplete
-            size="small"
-            loading={loadingLocations}
-            options={locations}
-            getOptionLabel={(option) => (option ? option.display_name : '')}
-            isOptionEqualToValue={(option, value) =>
-              option.place_id === value.place_id
-            }
-            value={selectedValue}
-            inputValue={inputValue}
-            onChange={onChangeAutoComplete}
-            onInputChange={onInputChange}
-            renderInput={(params) => (
-              <TextField {...params} placeholder="Buscar ciudad" />
-            )}
-            // Disabled native filtering of component suggested by MUI -> https://mui.com/material-ui/react-autocomplete/#search-as-you-type
-            filterOptions={(x) => x}
-          />
-        </AutocompleteContainer>
+        <TopAutocomplete />
       </JustifyToolbar>
       {loadingWeatherInfo && <LinearProgress />}
     </AppBar>
