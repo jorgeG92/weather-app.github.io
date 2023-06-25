7df134caaf244887e77abbd1d3c973358b79ae2c
jorgeG92 <jorgegc192@gmail.com>
2023-06-26 00:02:13 +0200

    Integrate store with UI for saved cities

--- src/components/CitiesList/AddCityButton/AddCityButton.tsx
+++ src/components/CitiesList/AddCityButton/AddCityButton.tsx
@@ -1,20 +1,49 @@
 import { Button, ButtonProps } from '@mui/material';
-import { FC, useState } from 'react';
+import { FC } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../../store';
+import { addCity, removeCity } from '../../../store/citiesList';
 
 const AddCityButton: FC = () => {
-  const [flag, setFlag] = useState(false);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const selectedLocation = useSelector(
+    (state: RootState) => state.locations.selectedLocation
+  );
+  const citiesList = useSelector((state: RootState) => state.citiesList);
+  const { todayInfo, nextDays } = useSelector(
+    (state: RootState) => state.weather
+  );
+
+  const isFavoriteCity = !!citiesList.find(
+    (city) => city.city.place_id === selectedLocation?.place_id
+  );
 
   const onClick: ButtonProps['onChange'] = () => {
-    setFlag(!flag);
+    if (selectedLocation) {
+      // In case if selected, remove from list
+      if (isFavoriteCity) dispatch(removeCity(selectedLocation.place_id));
+      // In case if not selected, add to the list
+      else {
+        if (todayInfo) {
+          const city = {
+            city: selectedLocation,
+            weatherInfo: { nextDays, todayInfo },
+          };
+          dispatch(addCity(city));
+        }
+      }
+    }
   };
 
   return (
     <Button
       variant="contained"
-      color={flag ? 'success' : 'warning'}
+      color={isFavoriteCity ? 'warning' : 'success'}
       onClick={onClick}
+      disabled={!(selectedLocation && todayInfo)}
     >
-      {flag ? 'Añadir' : 'Eliminar'} ciudad
+      {isFavoriteCity ? 'Eliminar' : 'Añadir'} ciudad
     </Button>
   );
 };
--- src/components/CitiesList/CitiesList.tsx
+++ src/components/CitiesList/CitiesList.tsx
@@ -1,35 +1,72 @@
 import styled from '@emotion/styled';
 import { Button, Grid, Typography } from '@mui/material';
 import { FC } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../store';
+import { GECODE_Location } from '../../api/locations';
+import { fetchWeatherInfo } from '../../store/weather';
+import { setLocation } from '../../store/locations';
+import { updateWeatherCityInfo } from '../../store/citiesList';
+import { WeatherInfoParsedResponse } from '../../api/weather';
 
 const FlexContainer = styled.div({
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
+  width: '100%',
 });
 
-const CitiesList: FC = () => (
-  <FlexContainer>
-    <Typography variant="h5" sx={{ marginBottom: 1 }}>
-      Ubicaciones guardadas
-    </Typography>
-    <Grid container spacing={2} direction={'row'} justifyContent={'center'}>
-      <Grid item sm={4} xs={12}>
-        <Button fullWidth variant="contained">
-          Ciudad 1
-        </Button>
+type CitiesListButtonProps = {
+  cityInfo: GECODE_Location;
+};
+
+const CitiesListButton: FC<CitiesListButtonProps> = ({ cityInfo }) => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const onClick = () => {
+    dispatch(setLocation(cityInfo));
+    dispatch(
+      fetchWeatherInfo({
+        lat: cityInfo.lat,
+        lon: cityInfo.lon,
+      })
+    ).then(({ payload }) => {
+      const weatherInfo = payload as Pick<
+        WeatherInfoParsedResponse,
+        'nextDays' | 'todayInfo'
+      >;
+      dispatch(
+        updateWeatherCityInfo({
+          id: cityInfo.place_id,
+          weatherInfo,
+        })
+      );
+    });
+  };
+
+  return (
+    <Button variant="contained" onClick={onClick} fullWidth>
+      <Typography noWrap>{cityInfo.display_name.split(', ')[0]}</Typography>
+    </Button>
+  );
+};
+
+const CitiesList: FC = () => {
+  const citiesList = useSelector((state: RootState) => state.citiesList);
+
+  return citiesList.length !== 0 ? (
+    <FlexContainer>
+      <Typography variant="h5" sx={{ marginBottom: 1 }}>
+        Ubicaciones guardadas
+      </Typography>
+      <Grid container spacing={2} direction={'row'} alignContent={'center'}>
+        {citiesList.map((item) => (
+          <Grid key={item.city.place_id} sm={4} item xs={12}>
+            <CitiesListButton cityInfo={item.city} />
+          </Grid>
+        ))}
       </Grid>
-      <Grid item sm={4} xs={12}>
-        <Button fullWidth variant="contained">
-          Ciudad 1
-        </Button>
-      </Grid>
-      <Grid item sm={4} xs={12}>
-        <Button fullWidth variant="contained">
-          Ciudad 1
-        </Button>
-      </Grid>
-    </Grid>
-  </FlexContainer>
-);
+    </FlexContainer>
+  ) : null;
+};
 export default CitiesList;
