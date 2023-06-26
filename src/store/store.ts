1f28d4afa5cf0d1e15aa04ff192aad0292fc0319
jorgeG92 <jorgegc192@gmail.com>
2023-06-26 19:59:01 +0200

    Add localStorage to redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,13 +3,40 @@ import locations from '../store/locations';
 import weather from '../store/weather';
 import citiesList from '../store/citiesList';
 
+const LOCAL_STORAGE_KEY = 'reduxState';
+
+// Save redux state in localStorage as JSON object
+const saveToLocalStorage = (state: RootState): void => {
+  try {
+    const stateToJSON = JSON.stringify(state);
+    localStorage.setItem(LOCAL_STORAGE_KEY, stateToJSON);
+  } catch (error) {
+    console.warn(error);
+  }
+};
+
+// Load state from localStorage
+const loadFromLocalStorage = () => {
+  try {
+    const localState = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (localState === null) return undefined;
+    return JSON.parse(localState);
+  } catch (error) {
+    console.warn(error);
+    return undefined;
+  }
+};
+
 const store = configureStore({
   reducer: {
     locations,
     weather,
     citiesList,
   },
+  preloadedState: loadFromLocalStorage(),
 });
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
